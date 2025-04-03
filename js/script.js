60868a33fd050bcc061dc31e9b900521f909659a
lau <168582504+HUlauLA@users.noreply.github.com>
2025-04-03 10:14:35 -0600

    Version incial

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function irAlMenu() {
+  window.location.href = "menu.html";
+}
+
+function deposito(){
+    window.location.href="Depositar.html";
+}
+
+function consulta(){
+    window.location.href="Consultar.html";
+}
+
+function historial(){
+    window.location.href="Historial.html";
+}
+
+function retiro(){
+    window.location.href="Retirar.html";
+}
+
+function servicio(){
+    window.location.href="Servicio.html";
+}
+
+function grafico(){
+    window.location.href="Grafico.html";
+}
+
+function salir(){
+    window.location.href="Login.html";
+}
