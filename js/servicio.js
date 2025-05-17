a041974fbe194a1d917efa38df160637dabd8a4c
Carlos-103 <alfaroespinozacarlosalberto@gmail.com>
2025-05-17 14:52:53 -0600

    Añadido Keypad para la opcion de pago de servicio

--- js/servicio.js
+++ js/servicio.js
@@ -1,3 +1,22 @@
+const montoInput = document.getElementById("montoServicio");
+// Keypad para ingresar el monto a pagar
+function agregarDigito(digito) {
+  if (montoInput && montoInput.value.length < 10) {
+    montoInput.value += digito;
+  }
+}
+//Esta función nos habilita el ingreso de datos decimales
+function agregarDecimal() {
+  if (montoInput && !montoInput.value.includes(".")) {
+    montoInput.value += ".";
+  }
+}
+//función para limpiar el display
+function limpiarMonto() {
+  if (montoInput) {
+    montoInput.value = "";
+  }
+}
 function pagarServicio() {
   //acá se declaran las variables
   const servicio = document.getElementById("servicio").value;
@@ -5,23 +24,34 @@ function pagarServicio() {
   const monto = parseFloat(document.getElementById("montoServicio").value);
   let saldo = parseFloat(localStorage.getItem("saldo")) || 500;
 
-  if (!servicio) {//si no se elige el servicio nos da un mensaje de advertencia
-    Swal.fire("Servicio requerido", "Seleccione un tipo de servicio", "warning");
+  if (!servicio) {
+    //si no se elige el servicio nos da un mensaje de advertencia
+    Swal.fire(
+      "Servicio requerido",
+      "Seleccione un tipo de servicio",
+      "warning"
+    );
     return;
   }
 
-  if (idServicio === "") {//si no se elige el servicio nos da un mensaje de advertencia
+  if (idServicio === "") {
+    //si no se elige el servicio nos da un mensaje de advertencia
     Swal.fire("ID requerido", "Debe ingresar el ID del servicio", "warning");
     return;
   }
 
-  if (isNaN(monto) || monto <= 0) {//si no se elige el servicio nos da un mensaje de advertencia
+  if (isNaN(monto) || monto <= 0) {
+    //si no se elige el servicio nos da un mensaje de advertencia
     Swal.fire("Monto inválido", "Ingrese un monto mayor a 0", "warning");
     return;
   }
 
   if (monto > saldo) {
-    Swal.fire("Saldo insuficiente", `Tu saldo es $${saldo.toFixed(2)}`, "error"); //acá se valida si el monto a pagar es mayor al saldo actual, entonces nos muestra un error
+    Swal.fire(
+      "Saldo insuficiente",
+      `Tu saldo es $${saldo.toFixed(2)}`,
+      "error"
+    ); //acá se valida si el monto a pagar es mayor al saldo actual, entonces nos muestra un error
     return;
   }
 
@@ -31,7 +61,12 @@ function pagarServicio() {
   // Guardar en historial
   let historial = JSON.parse(localStorage.getItem("historial")) || [];
   const fecha = new Date().toLocaleString();
-  historial.push({ tipo: `Servicio - ${servicio}`, monto: monto.toFixed(2), fecha, id: idServicio }); //El nombre con el cual se guarda el movimiento y que servicio fue
+  historial.push({
+    tipo: `Servicio - ${servicio}`,
+    monto: monto.toFixed(2),
+    fecha,
+    id: idServicio,
+  }); //El nombre con el cual se guarda el movimiento y que servicio fue
   localStorage.setItem("historial", JSON.stringify(historial));
 
   // Generar comprobante PDF
@@ -46,7 +81,11 @@ function pagarServicio() {
   doc.save(`comprobante_servicio_${servicio}.pdf`); //nombre con el que se genera el comprobante
 
   //mensaje de transacción exitosa y me redirige al menú
-  Swal.fire("¡Pago realizado!", `Servicio de ${servicio} pagado con éxito.`, "success").then(() => {
+  Swal.fire(
+    "¡Pago realizado!",
+    `Servicio de ${servicio} pagado con éxito.`,
+    "success"
+  ).then(() => {
     window.location.href = "menu.html";
   });
 }
@@ -54,14 +93,14 @@ function pagarServicio() {
 //botón volver al menú
 function cancelarPago() {
   Swal.fire({
-    title: '¿Cancelar el pago?',
-    text: 'Perderás los datos ingresados',
-    icon: 'question',
+    title: "¿Cancelar el pago?",
+    text: "Perderás los datos ingresados",
+    icon: "question",
     showCancelButton: true,
-    confirmButtonText: 'Sí, volver',
-    cancelButtonText: 'Seguir aquí',
-    confirmButtonColor: '#e74c3c',
-    cancelButtonColor: '#2ecc71'
+    confirmButtonText: "Sí, volver",
+    cancelButtonText: "Seguir aquí",
+    confirmButtonColor: "#e74c3c",
+    cancelButtonColor: "#2ecc71",
   }).then((result) => {
     if (result.isConfirmed) {
       window.location.href = "menu.html";
